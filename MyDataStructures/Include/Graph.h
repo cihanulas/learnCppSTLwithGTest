b8f8f9c3a0c520c995b2a030f751e8ed4299acd9
Cihan Ulas <cihanulas@yahoo.com>
2019-12-01 23:46:34 +0300

    Graph structure and related tests are added. Basic Directed and UnDirected graphs are implemented.

---
+++ MyDataStructures/Include/Graph.h
@@ -0,0 +1,87 @@
+﻿#pragma once
+
+#include <string>
+#include <unordered_map>
+//
+// class GNode {
+//  size_t id;
+//  std::string data;
+//};
+namespace Graph {
+using NodeWeightMap =
+    std::unordered_map<std::string /*to_node*/, double /*weight*/>;
+}
+
+struct IGraph {
+  virtual bool addVertex(const std::string& node) = 0;
+  virtual bool addEdge(const std::string& from, const std::string& to,
+                       double weight) = 0;
+  virtual bool IsVertexExist(const std::string& node) const = 0;
+  virtual Graph::NodeWeightMap find(const std::string& node) = 0;
+};
+
+class DirectedGraph : public IGraph {
+ public:
+  using NodeWeightMap = Graph::NodeWeightMap;
+  bool addVertex(const std::string& node) override;
+  bool virtual addEdge(const std::string& node1, const std::string& node2,
+                       double weight) override;
+  bool IsVertexExist(const std::string& node) const override;
+  NodeWeightMap find(const std::string& node) override {
+    auto it = m_node_connections.find(node);
+    if (it == m_node_connections.end()) return NodeWeightMap{};
+    return it->second;
+  }
+
+ private:
+  bool _addEdge(const std::string& node1, const std::string& node2,
+                double weight) {
+    auto it = m_node_connections.find(node1);
+    if (it == m_node_connections.end()) {
+      addVertex(node1);
+      m_node_connections.at(node1).emplace(node2, weight);
+    } else {
+      it->second.emplace(node2, weight);
+    };
+    return true;
+  };
+
+  /*
+  node1 - 100 - node2
+  node1 - 200 - node3
+  */
+
+  // Node -> connections map
+
+  std::unordered_map<std::string /*node_from*/, NodeWeightMap>
+      m_node_connections;
+};
+
+bool DirectedGraph::addVertex(const std::string& node) {
+  // insert the node, if there are already the new one will be ignored.
+  m_node_connections.emplace(node, NodeWeightMap{});
+  return true;
+};
+
+bool DirectedGraph::addEdge(const std::string& node1, const std::string& node2,
+                            double weight = 1) {
+  _addEdge(node1, node2, weight);
+  return true;
+};
+
+bool DirectedGraph::IsVertexExist(const std::string& node) const {
+  return m_node_connections.find(node) != m_node_connections.end();
+}
+
+class UnDirectedGraph : public DirectedGraph {
+ public:
+  bool addEdge(const std::string& node1, const std::string& node2,
+               double weight) override;
+};
+
+bool UnDirectedGraph::addEdge(const std::string& node1,
+                              const std::string& node2, double weight) {
+  DirectedGraph::addEdge(node1, node2, weight);
+  DirectedGraph::addEdge(node2, node1, weight);
+  return true;
+}
---
+++ MyDataStructuresUnitTests/TestGraph.cpp
@@ -0,0 +1,115 @@
+﻿#include "pch.h"
+
+#include "Graph.h"
+#include "gmock\gmock.h"
+
+MATCHER_P(GraphHasNode, value, "Is Vertex Exist in the Graph") {
+  bool res = arg.IsVertexExist(value);
+  return res;
+}
+
+MATCHER_P2(NodeRelation, node, linked_nodes, "Check Node Relations") {
+  Graph::NodeWeightMap it = arg.find(node);
+  return true;  // it == linked_nodes;
+}
+
+void AddEdges(IGraph& graph) {
+  graph.addEdge("0", "1", 1);
+  graph.addEdge("0", "2", 1);
+
+  graph.addEdge("1", "2", 2);
+  graph.addEdge("1", "3", 2);
+
+  graph.addEdge("2", "3", 3);
+  graph.addEdge("3", "0", 4);
+}
+
+/*
+  Create a graph with directed edge:
+  0: 1,2
+  1: 2,3
+  2: 1
+  3: 0
+*/
+void CheckDirectedGraph(IGraph& graph) {
+  Graph::NodeWeightMap node0Edges{{"1", 1}, {"2", 1}};
+  Graph::NodeWeightMap node1Edges{{"2", 2}, {"3", 2}};
+  Graph::NodeWeightMap node2Edges{{"3", 3}};
+  Graph::NodeWeightMap node3Edges{{"0", 4}};
+
+  EXPECT_EQ(graph.find("0"), node0Edges);
+  EXPECT_EQ(graph.find("1"), node1Edges);
+  EXPECT_EQ(graph.find("2"), node2Edges);
+  EXPECT_EQ(graph.find("3"), node3Edges);
+}
+
+/*
+  Create a graph like:
+  0: 1,2,3
+  1: 0,2,3
+  2: 0,1,3
+  3: 0,1,2
+*/
+
+void CheckUndirectedGraph(IGraph& graph) {
+  Graph::NodeWeightMap node0Edges{{"1", 1}, {"2", 1}, {"3", 4}};
+  Graph::NodeWeightMap node1Edges{{"0", 1}, {"2", 2}, {"3", 2}};
+  Graph::NodeWeightMap node2Edges{{"0", 1}, {"1", 2}, {"3", 3}};
+  Graph::NodeWeightMap node3Edges{{"0", 4}, {"1", 2}, {"2", 3}};
+
+  EXPECT_EQ(graph.find("0"), node0Edges);
+  EXPECT_EQ(graph.find("1"), node1Edges);
+  EXPECT_EQ(graph.find("2"), node2Edges);
+  EXPECT_EQ(graph.find("3"), node3Edges);
+}
+
+TEST(TestGraph, AddVertex) {
+  // Use initializer list
+  DirectedGraph graph;
+  graph.addVertex("0");
+  graph.addVertex("1");
+  ASSERT_THAT(graph, GraphHasNode("0"));
+  ASSERT_THAT(graph, GraphHasNode("1"));
+  EXPECT_FALSE(graph.IsVertexExist("2"));
+}
+
+// Directed Edge Tests
+TEST(TestGraph, AddDirectedEdgesWithVertexInitialization) {
+  DirectedGraph graph;
+  graph.addVertex("0");
+  graph.addVertex("1");
+  graph.addVertex("2");
+  graph.addVertex("3");
+  AddEdges(graph);
+  CheckDirectedGraph(graph);
+}
+TEST(TestGraph, AddDirectedEdgesWithoutVertexInitialization) {
+  DirectedGraph graph;
+  AddEdges(graph);
+  CheckDirectedGraph(graph);
+}
+
+// Undirected Edge Tests
+TEST(TestGraph, AddUnDirectedEdgesWithVertexInitialization) {
+  /*
+    Create a graph like:
+    0: 1,2,3
+    1: 0,2,3
+    2: 0,1,3
+    3: 0,1,2
+  */
+
+  UnDirectedGraph graph;
+  graph.addVertex("0");
+  graph.addVertex("1");
+  graph.addVertex("2");
+  graph.addVertex("3");
+  AddEdges(graph);
+  CheckUndirectedGraph(graph);
+}
+
+TEST(TestGraph, AddUnDirectedEdgesWithoutVertexInitialization) {
+  UnDirectedGraph graph;
+  AddEdges(graph);
+  CheckUndirectedGraph(graph);
+}
