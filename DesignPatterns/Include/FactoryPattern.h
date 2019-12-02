92b55ddb204db6c95a42b3458c7cfcd62947b964
Cihan Ulas <cihanulas@yahoo.com>
2019-12-03 01:37:00 +0300

    Factory Pattern and its tests are added.

---
+++ DesignPatterns/Include/FactoryPattern.h
@@ -0,0 +1,47 @@
+﻿#pragma once
+
+#include <string>
+
+// A Factory creates a different type of products using the same interface
+
+// First Define a product
+struct Product {
+  // product propteries
+  virtual std::string name() { return _name; };
+
+ protected:
+  std::string _name{"Product Default"};
+};
+
+class ProductA : public Product {
+ public:
+  ProductA() { _name = "Product A"; }
+  // std::string name() override { return _name; };
+};
+
+class ProductB : public Product {
+ public:
+  ProductB() { _name = "Product B"; }
+  // std::string name() override { return _name; };
+};
+
+// Creational
+// Factory creates products depending on a product type request
+class ProductFactory {
+ public:
+  enum class ProductType { A, B, C, D };
+
+  static Product getProduct(ProductType product) {
+    switch (product) {
+      case ProductType::A:
+        return ProductA();
+        break;
+      case ProductType::B:
+        return ProductB();
+        break;
+      default:  // others create default product
+        return Product();
+        break;
+    }
+  }
+};
---
+++ DesignPatternsTests/TestFactoryPattern.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+
+#include "FactoryPattern.h"
+
+TEST(TestFactoryPattern, GetProduct) {
+  // We have different internal logic, but user does not care.
+  // The consumer only specifies the type of the product
+
+  // usage with static method. ProductFactory::getProduct
+  Product productA = ProductFactory::getProduct(ProductFactory::ProductType::A);
+  Product productB = ProductFactory::getProduct(ProductFactory::ProductType::B);
+  EXPECT_EQ(productA.name(), "Product A");
+  EXPECT_EQ(productB.name(), "Product B");
+
+  // factory creates default product if product type is C and D.
+  ProductFactory factory;  // usage with object notation.
+  Product productC = factory.getProduct(ProductFactory::ProductType::C);
+  Product productD = factory.getProduct(ProductFactory::ProductType::D);
+  EXPECT_EQ(productC.name(), "Product Default");
+  EXPECT_EQ(productD.name(), "Product Default");
+}
